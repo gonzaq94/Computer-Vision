394aa6bb41a59602558d65ce86c67ee3ce02f9c2
gonzaq94 <gonza.quintana94@gmail.com>
2020-03-13 21:59:07 +0100

    TP5 Camera motion finished

---
+++ Camera Motion/drawlines.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+def drawlines(img1,img2,lines,pts1,pts2):
+
+    r = img1.shape[0]
+    c = img1.shape[1]
+    for r,pt1,pt2 in zip(lines,pts1,pts2):
+        color = tuple(np.random.randint(0,255,3).tolist())
+        x0,y0 = map(int, [0, -r[2]/r[1] ])
+        x1,y1 = map(int, [c, -(r[2]+r[0]*c)/r[1] ])
+        img1 = cv2.line(img1, (x0,y0), (x1,y1), color,1)
+        img1 = cv2.circle(img1,tuple(pt1),5,color,-1)
+        img2 = cv2.circle(img2,tuple(pt2),5,color,-1)
+    return img1,img2
---
+++ Camera Motion/simulated_images.py
@@ -0,0 +1,145 @@
+import cv2
+import numpy as np
+from drawlines import drawlines
+from scipy.spatial.transform import Rotation as Rot
+import matplotlib.pyplot as plt
+
+# Using cv2.imread() method 
+img1 = cv2.imread("data/simulated-pair-of-images/frame0000.jpg")
+img2 = cv2.imread("data/simulated-pair-of-images/frame0001.jpg")
+
+f = 554.254691191187
+Cx = 320.5	
+Cy = 240.5
+K1 = 0
+K2 = 0
+K3 = 0
+TD1 = 0
+TD2 = 0
+
+K = np.array([f, 0, Cx,0, f, Cy, 0,0,1]).reshape(3,3)
+distCoeffs = np.array([K1,K2,0,0])
+
+# undistort image
+img1_undis = cv2.undistort(img1,K,distCoeffs)
+img2_undis = cv2.undistort(img2,K,distCoeffs)
+
+# find keypoints and descriptors
+sift = cv2.xfeatures2d.SIFT_create()
+kp1, des1 = sift.detectAndCompute(img1_undis,None)
+kp2, des2 = sift.detectAndCompute(img2_undis,None)
+
+img1_sift = cv2.drawKeypoints(img1_undis,kp1,img1_undis)
+img2_sift = cv2.drawKeypoints(img2_undis,kp2,img2_undis)
+
+plt.imshow(img1_sift)
+plt.title('Img1 Keypoints')
+plt.show()
+cv2.imwrite('output_images/simulated-pair-of-images/img1_keypoints.jpg',img1_sift)
+
+plt.imshow(img2_sift)
+plt.title('Img2 Keypoints')
+plt.show()
+cv2.imwrite('output_images/simulated-pair-of-images/img2_keypoints.jpg',img2_sift)
+
+
+# Match descriptors.
+bf = cv2.BFMatcher()#crossCheck=True
+matches = bf.knnMatch(des1,des2, k=2)
+
+# Apply ratio test
+good = []
+
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+
+img3 = np.zeros(img1_undis.shape)
+img3 = cv2.drawMatchesKnn(img1_undis,kp1,img2_undis,kp2,good,img3,flags=2)
+
+plt.imshow(img3)
+plt.title('Matches')
+plt.show()
+cv2.imwrite('output_images/simulated-pair-of-images/matches.jpg',img3)
+
+#cv2.imwrite('img_matched.jpg',img3)
+
+good = []
+pts1 = []
+pts2 = []
+
+# ratio test as per Ldef drawlines(img1,img2,lines,pts1,pts2):
+
+for i,(m,n) in enumerate(matches):
+    if m.distance < 0.75*n.distance:
+        good.append(m)
+        pts2.append(kp2[m.trainIdx].pt)
+        pts1.append(kp1[m.queryIdx].pt)
+
+pts1 = np.int32(pts1)
+pts2 = np.int32(pts2)
+
+# calculate the F matrix
+
+F, mask = cv2.findFundamentalMat(pts1,pts2,cv2.FM_RANSAC)
+
+# We select only inlier points
+pts1 = pts1[mask.ravel()==1]
+pts2 = pts2[mask.ravel()==1]
+
+lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
+lines1 = lines1.reshape(-1,3)
+img1_epip,img6 = drawlines(img1_undis,img2_undis,lines1,pts1,pts2)
+
+plt.imshow(img1_epip)
+plt.title('Img1 epipolar lines')
+plt.show()
+cv2.imwrite('output_images/simulated-pair-of-images/img1_epipolar.jpg',img1_epip)
+
+lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2), 2,F)
+lines2 = lines2.reshape(-1,3)
+img2_epip,img7 = drawlines(img2_undis,img1_undis,lines2,pts2,pts1)
+
+plt.imshow(img2_epip)
+plt.title('Img2 epipolar lines')
+plt.show()
+cv2.imwrite('output_images/simulated-pair-of-images/img2_epipolar.jpg',img2_epip)
+
+#Essential matrix
+
+pts1 = []
+pts2 = []
+good = []
+
+# ratio test as per Ldef drawlines(img1,img2,lines,pts1,pts2):
+
+
+for i,(m,n) in enumerate(matches):
+    if m.distance < 0.65*n.distance:
+        good.append(m)
+        pts2.append(kp2[m.trainIdx].pt)
+        pts1.append(kp1[m.queryIdx].pt)
+    
+pts1 = np.int32(pts1)
+pts2 = np.int32(pts2)
+
+
+
+F, mask = cv2.findFundamentalMat(pts2,pts1,cv2.FM_RANSAC)
+
+E = np.matmul(np.matmul(np.transpose(K),F),K)
+R = np.zeros((3,3))
+t = np.zeros((3,1))
+cv2.recoverPose(E, pts2, pts1, K, R, t, mask);
+
+T = np.concatenate((np.transpose(R),-np.matmul(np.transpose(R),t)), axis=1)
+r = Rot.from_matrix(R)
+T = np.concatenate((T,np.array([0.,0.,0.,1.]).reshape(1,4)),axis=0)
+print('Relative motion matrix: ')
+print(T)
+
+
+angles = r.as_euler('xyz', degrees=True)
+print('Euler angles: ',angles)
+print('Translation direction: ')
+print(t)
